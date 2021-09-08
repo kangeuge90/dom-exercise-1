a2813ddcaa200ff4d469b7d1cc455c9698cc136a
Eugene Kang <kangeuge90@gmail.com>
2021-09-08 18:19:37 -0400

    first commit DNF

---
+++ script.js
@@ -0,0 +1,24 @@
+// When "Action!!!" button is clicked -- start main
+// already implemented in html
+function main() {
+    // 1.  Add the "big" class to the "grow-me" paragraph
+
+    // 2.  Remove the "big" class from the "shrink-me" paragraph
+
+    // 3.  Find all the <li>s and log their text content to the console.
+
+    // 4.  Set the href of the link to  "https://www.example.com" and update the text 
+    //      to say "somewhere" instead of "nowhere".
+
+    // 5.  Set the "display" CSS property of the "hide-me" paragraph to "none".
+
+    // 6.  Set the "display" CSS property of the "show-me" paragraph to "block".
+
+    // 7.  Get the text that the user enters into the "name" input box and use it to
+    //       set a welcome message in the <h1>, e.g., "Welcome Grant!".
+
+}
+
+
+
+// -------ANSWERED-----------
